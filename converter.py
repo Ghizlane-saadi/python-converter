6860d85da1edfd95c9fa4e2548aebf34f826d251
Python & Web Learner <saadi.ghizlane@outlook.com>
2026-09-12 22:24:20 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,8 +1,8 @@
 def celsius_to_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
-print("---   Temperaturwandler ---")
-celsius = float(input("Geben Sie den Wert in Prozent ein : "))
+print("---  Temperature Converter ---")
+celsius = float(input("Enter the value as a percentage:: "))
 fahrenheit = celsius_to_fahrenheit(celsius)
 
 print(f"{celsius}C = {fahrenheit}F")
